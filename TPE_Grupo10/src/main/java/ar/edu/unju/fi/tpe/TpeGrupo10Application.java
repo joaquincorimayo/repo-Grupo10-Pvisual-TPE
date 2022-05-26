3099cc4e38c49f2df95a6e0959c520d9d518b1f9
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-05-26 15:19:47 -0300

    Primer commit, estructura del proyecto.

---
+++ TPE_Grupo10/src/main/java/ar/edu/unju/fi/tpe/TpeGrupo10Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpeGrupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpeGrupo10Application.class, args);
+	}
+
+}
---
+++ TPE_Grupo10/src/test/java/ar/edu/unju/fi/tpe/TpeGrupo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpeGrupo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
